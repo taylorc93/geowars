635489666c6e8a88599144cb729992bf08c89e85
Connor Taylor <cmtaylor1123@gmail.com>
2015-05-30 23:54:55 -0400

    boilerplate html/js, initial readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Geowars
+
+This repository holds a HTML5 canvas clone of the popular Xbox arcade game Geometry Wars.
+
+Developed by Connor Taylor and Oliver Belanger
---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var canvas = document.getElementById("game-canvas");
+	var ctx = canvas.getContext("2d");
+
+	ctx.fillStyle = "green";
+	ctx.fillRect(10, 10, 100, 100);
+});
